9f1ff9d3d16207f4704bb9f9e6197f000c4a49ce
Vinicius Silva <viniciusgdr@outlook.com>
2024-08-19 20:55:52 -0300

    WIP

--- src/main/index.ts
+++ src/main/index.ts
@@ -37,7 +37,7 @@ async function loadCommands() {
 
   for (const folder of commandFolders) {
     const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts') || file.endsWith('.js'));
     for (const file of commandFiles) {
       const filePath = path.join(commandsPath, file);
       const command = (await import(filePath)).default;
